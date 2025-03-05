e14cd68cbf2dc498c754e044744f125b9a0eb64b
Remington Williams <rw53783n@pace.edu>
2025-03-05 10:56:06 -0500

    Update hashtable.py

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,27 +1,33 @@
 class HashTable:
     def __init__(self, n):
         ''' Initialize a hashtable of size n '''
-        pass
-
-    def hash(self):
+        self.size = n
+        self.table = [[] for _ in range(n)]  # create empty buckets
+    
+    def hash(self, item):
         ''' Compute a hash code for item '''
-        pass
+        return hash(item) % self.size  # get index using hash function
     
-    def add(self):
+    def add(self, item):
         ''' Add item to hash table '''
-        pass
-
-    def contains(self):
-        ''' Check if hashtable contains item '''
-        pass
+        index = self.hash(item)
+        if item not in self.table[index]:
+            self.table[index].append(item)  # append if not present
     
-    def remove(self):
+    def contains(self, item):
+        ''' Check if hashtable contains item '''
+        index = self.hash(item)
+        return item in self.table[index]  # check existence in bucket
+    
+    def remove(self, item):
         ''' Remove item from hashtable if it exists '''
-        pass
-
+        index = self.hash(item)
+        if item in self.table[index]:
+            self.table[index].remove(item)  # remove if found
+    
     def print_hashset(self):
         ''' Print the contents of each bucket in hashtable '''
-        pass
-
+        for i, bucket in enumerate(self.table):
+            print(f'Bucket {i}: {bucket}')  # print each bucket
 
 
