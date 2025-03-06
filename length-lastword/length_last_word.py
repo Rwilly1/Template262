f798ceba4cc32cdc15d478474c0b00be77a93ffc
tvkii <tvkelleyii@gmail.com>
2025-03-06 02:14:04 -0500

    Update length_last_word.py

--- length-lastword/length_last_word.py
+++ length-lastword/length_last_word.py
@@ -1,3 +1,4 @@
-def length_of_last_word(word):
+def length_of_last_word(word: str)-> int:
   ''' Returns the length of the last word in a string '''
-  return True
+  return len(word.split()[-1])
+  #return True
